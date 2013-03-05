344639a5113ebee65ad6a2632eec35e4bfe59ccd
Francesco Chicchiriccò <ilgrosso@apache.org>
2013-03-05 14:27:39 +0100

    [BASE-7] #comment Fixing license headers via com.google.code.maven-license-plugin:maven-license-plugin:1.4.0:format + apache-rat:0.9-SNAPSHOT configured but not enabled by default until RAT-129 is fixed

--- framework/src/test/resources/org/identityconnectors/common/StringUtilTests_template.js
+++ framework/src/test/resources/org/identityconnectors/common/StringUtilTests_template.js
@@ -1,3 +1,25 @@
+/*
+ * ====================
+ *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2008-2009 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2011-2013 Tirasa. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License("CDDL") (the "License"). You may not use this file
+ * except in compliance with the License.
+ *
+ * You can obtain a copy of the License at https://oss.oracle.com/licenses/CDDL
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * When distributing the Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at https://oss.oracle.com/licenses/CDDL.
+ * If applicable, add the following below this CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * ====================
+ */
 //
 // Declare all the graphs..
 //
