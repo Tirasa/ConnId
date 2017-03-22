3a5bc3bbfeb447c0d4c91e9c2e3cdbd49c738aa4
Radovan Semancik <radovan.semancik@evolveum.com>
2017-03-22 12:12:56 +0100

    [BASE-59] Initial design of delta-based update operation.

---
+++ java/connector-framework/src/main/java/org/identityconnectors/framework/common/objects/AttributeDelta.java
@@ -0,0 +1,196 @@
+/*
+ * ====================
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2017 Evolveum. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License("CDDL") (the "License").  You may not use this file
+ * except in compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://opensource.org/licenses/cddl1.php
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * When distributing the Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://opensource.org/licenses/cddl1.php.
+ * If applicable, add the following below this CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * ====================
+ */
+package org.identityconnectors.framework.common.objects;
+
+import static org.identityconnectors.framework.common.objects.NameUtil.nameHashCode;
+import static org.identityconnectors.framework.common.objects.NameUtil.namesEqual;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.identityconnectors.common.CollectionUtil;
+import org.identityconnectors.common.StringUtil;
+
+/**
+ * <p>
+ * Specifies a modification of a single {@link Attribute}. The delta is often a description
+ * of a relative modification of the attribute. It describes values that are added
+ * and removed. But it may also define an absolute modification: values that are replaced.
+ * </p>
+ * <p>
+ * The added/removed/replaced attributes are defined by a separate value lists.
+ * The valuesToAdd list defines the new attribute values to add to existing values.
+ * The valuesToRemove list defines the attribute values that will be removed from existing attribute values.
+ * The valuesToReplace list defines the new attribute values. In that case existing attribute values will be
+ * removed and new attribute values from the valuesToReplace list will be placed in the attribute.
+ * The delta may have add and remove lists at the same time. But if replace list is specified then no other
+ * list may be present.
+ * Empty list is not the same as null list, especially in the replace case. Null list means that there is no
+ * modification of that particular type (add/remove/replace). Empty list means that there is a modification
+ * of that particular type, but it does not include any value. This distinction is important especially for
+ * the replace case. Delta with empty valuesToReplace list means that all existing values of an attribute
+ * should be removed, but no new value is to be set in the attribute. The resulting state is attribute with
+ * no values.
+ * </p>
+ * <p> 
+ * The delta does not guarantee ordering of the values. It is not guaranteed that the added attributes will
+ * be appended at the end. Nor is the resulting order of values after application of remove delta guaranteed.
+ * This behavior is connector-specific.
+ * </p>
+ * <p>
+ * Terminology note: The term "delete" would be better than "remove", especially because "remove" may be
+ * easily confused with "replace". But the framework is already using the term "remove", so we have preferred
+ * naming consistency in this case.
+ * </p>
+ * <p>
+ * The {@link AttributeDeltaBuilder} should be used to construct an instance of AttributeDelta.
+ * </p>
+ *
+ * @author Radovan Semancik
+ * @since 1.4.3
+ */
+public class AttributeDelta {
+
+    /**
+     * Name of the attribute
+     */
+    private final String name;
+
+    /**
+     * Attribute values to add
+     */
+    private final List<Object> valuesToAdd;
+    
+    /**
+     * Attribute values to remove
+     */
+    private final List<Object> valuesToRemove;
+
+    /**
+     * Attribute values to replace
+     */
+    private final List<Object> valuesToReplace;
+
+    /**
+     * Create an attribute delta.
+     */
+    AttributeDelta(String name, List<Object> valuesToAdd, List<Object> valuesToRemove, List<Object> valuesToReplace) {
+        if (StringUtil.isBlank(name)) {
+            throw new IllegalArgumentException("Name must not be blank!");
+        }
+        // make this case insensitive
+        this.name = name;
+        // sanity
+        if (valuesToReplace != null && (valuesToAdd != null || valuesToRemove != null)) {
+        	throw new IllegalArgumentException("Delta of attribute '"+name+"' may be either replace or add/remove but not both at the same time");
+        }
+        // copy to prevent corruption..
+        this.valuesToAdd = (valuesToAdd == null) ? null : CollectionUtil.newReadOnlyList(valuesToAdd);
+        this.valuesToRemove = (valuesToRemove == null) ? null : CollectionUtil.newReadOnlyList(valuesToRemove);
+        this.valuesToReplace = (valuesToReplace == null) ? null : CollectionUtil.newReadOnlyList(valuesToReplace);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<Object> getValuesToAdd() {
+        return (this.valuesToAdd == null) ? null : Collections.unmodifiableList(this.valuesToAdd);
+    }
+
+    public List<Object> getValuesToRemove() {
+        return (this.valuesToRemove == null) ? null : Collections.unmodifiableList(this.valuesToRemove);
+    }
+
+    public List<Object> getValuesToReplace() {
+        return (this.valuesToReplace == null) ? null : Collections.unmodifiableList(this.valuesToReplace);
+    }
+
+    /**
+     * Determines if the 'name' matches this {@link AttributeDelta}.
+     */
+    public boolean is(String name) {
+        return namesEqual(this.name, name);
+    }
+
+    @Override
+    public final int hashCode() {
+        return nameHashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        // poor man's consistent toString impl..
+        StringBuilder bld = new StringBuilder();
+        bld.append("Attribute: ");
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        map.put("Name", getName());
+        map.put("ValuesToAdd", getValuesToAdd());
+        map.put("ValuesToRemove", getValuesToRemove());
+        map.put("ValuesToReplace", getValuesToReplace());
+        extendToStringMap(map);
+        bld.append(map);
+        return bld.toString();
+    }
+
+    protected void extendToStringMap(final Map<String, Object> map) {
+        // Nothing to do here. Just for use in sublcasses.
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        // test identity
+        if (this == obj) {
+            return true;
+        }
+        // test for null..
+        if (obj == null) {
+            return false;
+        }
+        // test that the exact class matches
+        if (!(getClass().equals(obj.getClass()))) {
+            return false;
+        }
+        // test name field..
+        final AttributeDelta other = (AttributeDelta) obj;
+        if (!is(other.name)) {
+            return false;
+        }
+
+        if (!CollectionUtil.equals(valuesToAdd, other.valuesToAdd)) {
+            return false;
+        }
+        
+        if (!CollectionUtil.equals(valuesToRemove, other.valuesToRemove)) {
+            return false;
+        }
+        
+        if (!CollectionUtil.equals(valuesToReplace, other.valuesToReplace)) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ java/connector-framework/src/main/java/org/identityconnectors/framework/spi/operations/UpdateDeltaOp.java
@@ -0,0 +1,104 @@
+/*
+ * ====================
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2017 Evolveum. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License("CDDL") (the "License").  You may not use this file
+ * except in compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://opensource.org/licenses/cddl1.php
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * When distributing the Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://opensource.org/licenses/cddl1.php.
+ * If applicable, add the following below this CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * ====================
+ */
+package org.identityconnectors.framework.spi.operations;
+
+import java.util.Set;
+
+import org.identityconnectors.framework.common.objects.AttributeDelta;
+import org.identityconnectors.framework.common.objects.ObjectClass;
+import org.identityconnectors.framework.common.objects.OperationOptions;
+import org.identityconnectors.framework.common.objects.Uid;
+
+/**
+ * <p>
+ * Proper implementation of {@link UpdateOp} and {@link UpdateAttributeValuesOp}.
+ * This operation updates the values in relative or absolute way. Modification
+ * of each attribute is described by {@link AttributeDelta}.
+ * </p>
+ * <p>
+ * This operation is meant as a complete replacement of {@link UpdateOp} and 
+ * {@link UpdateAttributeValuesOp} that were poorly designed in the early 
+ * framework versions. The old operations have not provided any practical
+ * way how to execute complex attribute changes in an efficient or atomic
+ * way. The presence of a single operation in {@link UpdateOp} could not
+ * support any kind of relative changes. The two operations added in
+ * {@link UpdateAttributeValuesOp} did not really solve the problem either.
+ * The fact that there are two operations made it impossible to implement
+ * all scenarios due to operation ordering. E.g. invoking remove operation
+ * first and add operation second will end up with an error when working
+ * with mandatory multi-value attribute. On the other hand invoking add
+ * operation first and remove operation second will cause issues with
+ * attributes that represent some limited entitlements (e.g. groups that
+ * may be assigned only once). Also the fact that the old update operations
+ * returns just the Uid make it very hard to implement an efficient and
+ * reliable connector. E.g. it is a common case that the name of the object
+ * changes when some of the attributes are changed. The old update operations
+ * provided no way how to indicate this change even if the connector was aware
+ * of the change.
+ * </p> 
+ * 
+ * @author Radovan Semancik
+ * @since 1.4.3
+ */
+public interface UpdateDeltaOp extends SPIOperation {
+
+    /**
+     * <p>
+     * Update the object specified by the {@link ObjectClass} and {@link Uid},
+     * modifying the values according to the attribute deltas.
+     * </p>
+     * <p>
+     * The connector is supposed to return side-effect changes as a return value
+     * from this operation. E.g. if the modification of some of the
+     * attributes changed other attribute then these changes should be returned.
+     * The connector must return a new value of primary identifier (Uid) if it is
+     * changed. But the connector should return other changes only if the connector
+     * has an efficient way how to detect them. Connector is not supposed to return
+     * all side-effect changes if it does not know about them or if additional
+     * operation is required to fetch them.
+     * </p>
+     *
+     * @param objclass
+     *            the type of object to modify. Will never be null.
+     * @param uid
+     *            the uid of the object to modify. Will never be null.
+     * @param modifications
+     *            set of attribute deltas. Each delta describes modification
+     *            of one attribute. Each attribute will be in the set at most once. 
+     *            The set will never be null.
+     * @param options
+     *            additional options that impact the way this operation is run.
+     *            Will never be null.
+     * @return the set of modifications that were a side-effect of the primary modifications
+     *            specified in the modifications parameters.
+     * @throws org.identityconnectors.framework.common.exceptions.UnknownUidException
+     *             if the {@link Uid} does not exist on the resource.
+     * @throws org.identityconnectors.framework.common.exceptions.InvalidAttributeValueException
+     *             if any of the specified values is not appropriate for the attribute,
+     *             if the delta execution would result in violation of the schema,
+     *             if the result would be missing mandatory attribute and in similar cases.
+     */
+    public Set<AttributeDelta> updateDelta(ObjectClass objclass, Uid uid, Set<AttributeDelta> modifications,
+            OperationOptions options);
+
+}
